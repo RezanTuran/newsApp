62f8a4055f2d362a33e14f5d435264e980ff0abf
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2022-09-05 15:08:39 +0200

    render data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Axios from 'axios';
 
 function App() {
+  const [news, setNews] = useState([]);
   useEffect(() => {
     Axios.get(
       `https://newsapi.org/v2/top-headlines?country=se&category=sport&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
     )
       .then((Response: any) => {
+        setNews(Response.data.articles);
         console.table(Response.data.articles);
       })
       .catch((err) => {
@@ -17,7 +19,25 @@ function App() {
 
   return (
     <div>
-      <h1>App</h1>
+      {news.map((news: any, index: any) => (
+        <div
+          key={index}
+          style={{
+            width: '90%',
+            border: '2px solid black',
+            margin: '10px',
+            padding: '10px',
+            backgroundColor: 'tomato',
+          }}
+        >
+          <h4>{news.title}</h4>
+          <p>{news.description}</p>
+          <img src={news.urlToImage} alt="ss" style={{ width: '30%' }} />
+          <h4>{news.author}</h4>
+          <p>{news.publishedAt}</p>
+          <p>{news.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
