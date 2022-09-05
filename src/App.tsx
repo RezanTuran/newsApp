1c90a9382952bc1de3baa176b01dfacc6984b0c6
RezanTuran <39722976+RezanTuran@users.noreply.github.com>
2022-09-05 14:55:46 +0200

    fetch data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    Axios.get(
+      `https://newsapi.org/v2/top-headlines?country=se&category=sport&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+    )
+      .then((Response: any) => {
+        console.table(Response.data.articles);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
